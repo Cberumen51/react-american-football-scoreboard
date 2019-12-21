3d04fd0f9bf049b5675de5044c878ae2d8da25a8
Cberumen51 <c.berumen51@gmail.com>
2019-12-20 16:13:21 -0800

    added timer stuff

--- src/App.js
+++ src/App.js
@@ -1,12 +1,35 @@
 //TODO: STEP 1 - Import the useState hook.
-import React, {useState} from "react";
+import React, {useState, useEffect} from "react";
 import "./App.css";
 import BottomRow from "./BottomRow";
+import useClock from 'use-clock'
 
 function App() {
   //TODO: STEP 2 - Establish your applictaion's state with some useState hooks.  You'll need one for the home score and another for the away score.
   const [homeStart, homeScore] = useState (0);  
   const [awayStart, awayScore] = useState (0);
+  const [seconds, setSeconds] = useState(900000);
+  const[isActive, setIsActive] =useState(false);
+
+  function toggle() {
+    setIsActive(!isActive);
+  }
+  function reset() {
+    setSeconds(900000);
+    setIsActive(false);
+  }
+
+  useEffect(() => {
+    let interval = null;
+    if (isActive) {
+      interval = setInterval(() => {
+        setSeconds(seconds => seconds - 1);
+      }, 1000);
+    } else if (!isActive && seconds !== 900000) {
+      clearInterval(interval);
+    }
+    return () => clearInterval(interval);
+  }, [isActive, seconds]);
 
   const homeTwoPointer = (e) => {
      homeScore(homeStart + 2)
@@ -26,17 +49,20 @@ const awayThreePointer = (e) => {
 const awayFreeThrow = (e) => {
   awayScore(awayStart + 1)
 }
-  const awayTouchdown = (e) => {
-    awayScore(awayStart + 7) 
-  }
-  const homeFieldGoal = (e) => {
-    homeScore(homeStart + 3)
-  }
-  const awayFieldGoal = (e) => {
-    awayScore(awayStart + 3)
-  }
+
+
+
+
+
   return (
     <div className="container">
+        <div className="app">
+      <div className="time">
+      </div>
+      <div className="row">
+        
+      </div>
+    </div>
       <section className="scoreboard">
         <div className="topRow">
           <div className="home">
@@ -46,7 +72,7 @@ const awayFreeThrow = (e) => {
 
             <div className="home__score">{homeStart}</div>
           </div>
-          <div className="timer">00:03</div>
+          <div className="timer">{seconds}</div>
           <div className="away">
             <h2 className="away__name">Celtics</h2>
             <div className="away__score">{awayStart}</div>
@@ -57,15 +83,32 @@ const awayFreeThrow = (e) => {
       <section className="buttons">
         <div className="homeButtons">
           {/* TODO STEP 4 - Now we need to attach our state setter functions to click listeners. */}
+          <button className="homeButtons__Freethrow" onClick={homeFreeThrow}>Home Free Throw</button>
+
           <button className="homeButtons__twoPointer" onClick={homeTwoPointer}>Home Two Pointer</button>
           <button className="homeButtons__threepointer" onClick={homeThreePointer}>Home Three Pointer</button>
-          <button className="homeButtons__Freethrow" onClick={homeFreeThrow}>Home Free Throw</button>
+          <div className="shotClockButtons">
+    <button className={`button button-primary button-primary-${isActive ? 'active' : 'inactive'}`} onClick={toggle}>Shot Clock
+          {isActive ? ' Pause' : ' Start'}
+        </button>
+        <button className="button" onClick={reset}>
+          Shot Clock Reset
+        </button>
+        </div>
         </div>
         <div className="awayButtons">
+        <button className="homeButtons__freeThrow" onClick={awayFreeThrow}>Away Free Throw </button>
+
           <button className="awayButtons__twoPointer" onClick={awayTwoPointer}>Away Two Pointer</button>
           <button className="awayButtons__threePointer" onClick={awayThreePointer}>Away Three Pointer</button>
-          <button className="homeButtons__freeThrow" onClick={awayFreeThrow}>Away Free Throw </button>
+          <button className={`button button-primary button-primary-${isActive ? 'active' : 'inactive'}`} onClick={toggle}>Game Clock 
+          {isActive ? ' Pause' : ' Start'}
+        </button>
+        <button className="button" onClick={reset}>
+          Game Clock Reset
+        </button>
         </div>
+       
       </section>
     </div>
   );
--- src/BottomRow.js
+++ src/BottomRow.js
@@ -1,12 +1,46 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import "./App.css";
 
 const BottomRow = () => {
+  const [seconds, setSeconds] = useState(24);
+  const[isActive, setIsActive] =useState(false);
+
+  function toggle() {
+    setIsActive(!isActive);
+  }
+  function reset() {
+    setSeconds(24);
+    setIsActive(false);
+  }
+
+  useEffect(() => {
+    let interval = null;
+    if (isActive) {
+      interval = setInterval(() => {
+        setSeconds(seconds => seconds - 1);
+      }, 1000);
+    } else if (!isActive && seconds !== 24) {
+      clearInterval(interval);
+    }
+    return () => clearInterval(interval);
+  }, [isActive, seconds]);
+
+
   return (
+    <div className="container">
+
+
+    <div className="app">
+      <div className="time">
+      </div>
+      <div className="row">
+        
+      </div>
+    </div>
     <div className="bottomRow">
       <div className="down">
         <h3 className="down__title">Shotclock</h3>
-        <div className="down__value">03</div>
+  <div className="down__value">{seconds}</div>
       </div>
       <div className="toGo">
         <h3 className="toGo__title">Home Fouls</h3>
@@ -14,13 +48,24 @@ const BottomRow = () => {
       </div>
       <div className="ballOn">
         <h3 className="ballOn__title">Away Fouls</h3>
-        <div className="ballOn__value">21</div>
+        <div className="ballOn__value">14</div>
       </div>
       <div className="quarter">
-        <h3 className="quarter__title">Period</h3>
-        <div className="quarter__value">4</div>
+        <h3 className="quarter__title">Quarter</h3>
+        <div className="quarter__value">1</div>
       </div>
     </div>
+    {/* <section className="buttons"> */}
+    {/* <div className="shotClockButtons"> */}
+    <button className={`button button-primary button-primary-${isActive ? 'active' : 'inactive'}`} onClick={toggle}>Shot Clock
+          {isActive ? ' Pause' : ' Start'}
+        </button>
+        <button className="button" onClick={reset}>
+          Shot Clock Reset
+        </button>
+        {/* </div> */}
+         {/* </section> */}
+     </div>
   );
 };
 
